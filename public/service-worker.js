645229dd7a68b352b7a31c1ec73b8be0250b8867
Umera <umeramalek796@gmail.com>
2021-11-17 22:01:21 -0800

    added files to cache in service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+console.log("Service worker connected ")
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/js/index.js",
+    "/js/indexedDB.js",
+    "/manifest.json",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
