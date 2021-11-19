bf43ac039957d6a4d33e86654e1b105d5551e774
Umera <umeramalek796@gmail.com>
2021-11-18 21:25:27 -0800

    dataBase function

--- public/js/indexedDb.js
+++ public/js/indexedDb.js
@@ -25,3 +25,29 @@ function saveRecord(record) {
 
     store.add(record);
 };
+
+function dataBase(){
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function(){
+        if (getAll.result.length > 0){
+            fetch("api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                header: {
+                    Accept : "application/json, text/plin, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            })
+        }
+    };
+}
