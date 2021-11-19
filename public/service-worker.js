6f71e392d77ffbe34880ef3f73f893f41a2e2d10
Umera <umeramalek796@gmail.com>
2021-11-18 22:39:27 -0800

    install caches

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,3 +12,13 @@ const FILES_TO_CACHE = [
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
 ];
+// 1. install
+self.addEventListener("install", function(event){
+    event.waitUntil(
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction")));
+        event.waitUntil(
+            caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)));
+        //forces the waiting service worker to become the active service worker - reloads the service worke  
+        self.skipWaiting();
+});
+
