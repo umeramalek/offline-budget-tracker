be861dd508a37062fb88e2d6997fcc99c2ddf032
Umera <umeramalek796@gmail.com>
2021-11-18 21:19:15 -0800

    on upgrade function

---
+++ public/js/indexedDb.js
@@ -0,0 +1,10 @@
+let db;
+
+const request = indexedDB.open("budget-tracker",1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore("pending", {autoIncrement:true})
+};
+
+
